5f3dd9ee6071cf8d019067dd8c5e6f270555fde3
Nika Sharmazanashvili <nsharmazanashvili@ip.local>
2025-04-30 16:59:16 +0400

    changed tables to views to decrease response time, added nolock to query

--- app/services/db_service.py
+++ app/services/db_service.py
@@ -4,27 +4,28 @@ import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 TABLE_NAMES = [
-    "vw_Last_10_Withdrawals_Tumbet",
-    "vw_Top_Profitable_Players_1Day_Sum_Tumbet",
-    "vw_Top_Profitable_Players_1Week_Sum_Tumbet",
-    "vw_Top_Profitable_Players_1Month_Sum_Tumbet",
-    "vw_Top_Paying_Players_1Day_Sum_Tumbet",
-    "vw_Top_Paying_Players_1Week_Sum_Tumbet",
-    "vw_Top_Paying_Players_1Month_Sum_Tumbet",
-    "vw_Top_Players_Who_Made_Most_of_The_Money_1Day_Sum_Tumbet",
-    "vw_Top_Players_Who_Made_Most_of_The_Money_1Week_Sum_Tumbet",
-    "vw_Top_Players_Who_Made_Most_of_The_Money_1Month_Sum_Tumbet",
-    "vw_Latest_10_winners_Slots_Tumbet",
-    "vw_Highest_10_Earners_Slots_Tumbet",
-    "vw_Top_Payer_Providers_Slots_1Week_Sum_Tumbet",
-    "vw_Top_Paying_Slots_1Week_Sum_Tumbet",
+    "tbl_vw_Last_10_Withdrawals_Tumbet",
+    "tbl_vw_Top_Profitable_Players_1Day_Sum_Tumbet",
+    "tbl_vw_Top_Profitable_Players_1Week_Sum_Tumbet",
+    "tbl_vw_Top_Profitable_Players_1Month_Sum_Tumbet",
+    "tbl_vw_Top_Paying_Players_1Day_Sum_Tumbet",
+    "tbl_vw_Top_Paying_Players_1Week_Sum_Tumbet",
+    "tbl_vw_Top_Paying_Players_1Month_Sum_Tumbet",
+    "tbl_vw_Top_Players_Who_Made_Most_of_The_Money_1Day_Sum_Tumbet",
+    "tbl_vw_Top_Players_Who_Made_Most_of_The_Money_1Week_Sum_Tumbet",
+    "tbl_vw_Top_Players_Who_Made_Most_of_The_Money_1Month_Sum_Tumbet",
+    "tbl_vw_Latest_10_Winners_Slots_Tumbet",
+    "tbl_vw_Highest_10_Earners_Slots_Tumbet",
+    "tbl_vw_Top_Payer_Providers_Slots_1Week_Sum_Tumbet",
+    "tbl_vw_Top_Paying_Slots_1Week_Sum_Tumbet",
 ]
 
 def fetch_table(table_name):
     with get_connection() as conn:
-        query = f"SELECT * FROM ReportDB.BIDS.{table_name}"
+        query = f"SELECT * FROM ReportDB.BIDS.{table_name} (nolock)"
         df = pd.read_sql(query, conn)
-        return table_name[3:-7], df.to_dict(orient="records")
+        print(table_name, "   done")
+        return table_name[7:-7], df.to_dict(orient="records")
 
 def fetch_all_tables():
     start_time = time.time()
